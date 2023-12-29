ae5d5e060ffeaedfcda2012dd76b674baa8ecdd1
Divyam Garg <dvm.garg26@gmail.com>
2023-12-29 14:52:18 +0530

    files updated

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -11,7 +11,7 @@ import { useSelector } from 'react-redux'
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
-    <div className='mt-8 md:mt-12 flex flex-col gap-10 relative z-10 xl:-mt-[210px]'>
+    <div className='mt-8 md:mt-12 flex flex-col gap-10 relative z-10 xl:-mt-[270px]'>
       <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
       <MovieList title={"Popular"} movies={movies.popularMovies} />
       <MovieList title={"Top Rated"} movies={movies.topRatedMovies} />
